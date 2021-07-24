07fd3e7479820cf8a6f303cb8f7f76f7095ff3c9
Tabroid <pengc2019@gmail.com>
2021-07-24 16:39:11 -0500

    rename a few files

--- Source/AlphaOne/Private/buildings/Tower.cpp
+++ Source/AlphaOne/Private/buildings/Tower.cpp
@@ -1,10 +1,10 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+#include "buildings/Tower.h"
 #include "Components/CapsuleComponent.h"
 #include "Components/WidgetComponent.h"
 #include "Kismet/GameplayStatics.h"
-#include "building/Tower.h"
-#include "Widget/HealthBar.h"
+#include "widgets/HealthBar.h"
 #include "GameFramework/Character.h"
 
 // Sets default values
--- Source/AlphaOne/Private/widgets/HealthBar.cpp
+++ Source/AlphaOne/Private/widgets/HealthBar.cpp
@@ -1,17 +1,17 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "Widget/HealthBar.h"
+#include "widgets/HealthBar.h"
 #include "Components/ProgressBar.h"
-#include "building/Tower.h"
 #include "Components/TextBlock.h"
+#include "buildings/Tower.h"
 
-void UHealthBar::SetOwnerActor(ATower* Owner) 
+void UHealthBar::SetOwnerActor(ATower* Owner)
 {
     OwnerActor = Owner;
 }
 
-void UHealthBar::NativeTick(const FGeometry& MyGeometry, float InDeltaTime) 
+void UHealthBar::NativeTick(const FGeometry& MyGeometry, float InDeltaTime)
 {
     Super::NativeTick(MyGeometry, InDeltaTime);
 
