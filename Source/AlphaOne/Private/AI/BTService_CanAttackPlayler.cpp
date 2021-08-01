41b7f88cbc00a5b7195ec147652f831f20d3334a
Tabroid <pengc2019@gmail.com>
2021-08-01 14:38:10 -0500

    clean up some AI code

--- Source/AlphaOne/Private/AI/BTService_CanAttackPlayler.cpp
+++ Source/AlphaOne/Private/AI/BTService_CanAttackPlayler.cpp
@@ -8,24 +8,23 @@
 #include "GameFramework/Character.h"
 #include "AI/BTService_CanAttackPlayler.h"
 
-UBTService_CanAttackPlayler::UBTService_CanAttackPlayler() 
+UBTService_CanAttackPlayler::UBTService_CanAttackPlayler()
 {
 	NodeName = TEXT("Can Attack Player");
 }
 
-void UBTService_CanAttackPlayler::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) 
+void UBTService_CanAttackPlayler::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
 {
 	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
 
-	//check if the enemy can attack the player, if yes, set the blackboard key to the player
-	//if not, clear the blackboard key
+	// check if the enemy can attack the player, if yes, set the blackboard key to the player
+	// if not, clear the blackboard key
 	if (OwnerComp.GetAIOwner() == nullptr) return;
 
     APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
 	APawn* ControlledPawn = OwnerComp.GetAIOwner()->GetPawn();
     if (!PlayerPawn || !ControlledPawn) return;
 
-	
 	float Distance = FVector::Dist(ControlledPawn->GetActorLocation(), PlayerPawn->GetActorLocation());
 
 	//FVector HeadVector = (Cast<ACharacter>(ControlledPawn)->GetMesh()->GetSocketRotation(TEXT("head"))).Vector();
@@ -38,14 +37,12 @@ void UBTService_CanAttackPlayler::TickNode(UBehaviorTreeComponent& OwnerComp, ui
 
 	angle *= (180./(UKismetMathLibrary::Atan(1.)*4.));
 
-	UE_LOG(LogTemp, Warning, TEXT("%f"), angle);
+	// UE_LOG(LogTemp, Warning, TEXT("%f"), angle);
 
-    if (OwnerComp.GetAIOwner()->LineOfSightTo(PlayerPawn) && angle < 60. && Distance < 1000.){
+    if (OwnerComp.GetAIOwner()->LineOfSightTo(PlayerPawn) && angle < 60. && Distance < 1000.) {
         OwnerComp.GetBlackboardComponent()->SetValueAsObject(GetSelectedBlackboardKey(), PlayerPawn);
-    }
-    else{
+    } else {
         OwnerComp.GetBlackboardComponent()->ClearValue(GetSelectedBlackboardKey());
     }
-
 }
 
--- Source/AlphaOne/Private/AI/BTTask_Attack.cpp
+++ Source/AlphaOne/Private/AI/BTTask_Attack.cpp
@@ -7,27 +7,27 @@
 
 
 
-UBTTask_Attack::UBTTask_Attack() 
+UBTTask_Attack::UBTTask_Attack()
 {
 	NodeName = TEXT("Attack");
 }
 
-EBTNodeResult::Type UBTTask_Attack::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) 
+EBTNodeResult::Type UBTTask_Attack::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
 	Super::ExecuteTask(OwnerComp, NodeMemory);
-    
-    if (OwnerComp.GetAIOwner() == nullptr){
+
+    if (OwnerComp.GetAIOwner() == nullptr) {
         return EBTNodeResult::Failed;
     }
-    //call the attack function of the pawn 
+    //call the attack function of the pawn
     ACharacterBase* Character = Cast<ACharacterBase>(OwnerComp.GetAIOwner()->GetPawn());
 
-    if (Character == nullptr){
+    if (Character == nullptr) {
         return EBTNodeResult::Failed;
     }
 
     Character->Attack();
-    
+
     return EBTNodeResult::Succeeded;
 }
 
