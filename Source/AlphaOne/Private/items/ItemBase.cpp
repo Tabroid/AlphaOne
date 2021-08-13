db84d7249a5a7d2e9d279e8948fcb4879f8ad4cf
Tabroid <pengc2019@gmail.com>
2021-08-13 12:02:58 -0500

    add allowed interactions flag to item base

--- Source/AlphaOne/Private/items/ItemBase.cpp
+++ Source/AlphaOne/Private/items/ItemBase.cpp
@@ -4,15 +4,6 @@
 #include "items/ItemBase.h"
 
 
-bool UItemBase::IsConsumable() const
-{
-	if (MaxCount <= 0)
-	{
-		return true;
-	}
-	return false;
-}
-
 FString UItemBase::GetIdentifierString() const
 {
 	return GetPrimaryAssetId().ToString();
@@ -23,4 +14,13 @@ FPrimaryAssetId UItemBase::GetPrimaryAssetId() const
 	// This is a DataAsset and not a blueprint so we can just use the raw FName
 	// For blueprints you need to handle stripping the _C suffix
 	return FPrimaryAssetId(ItemType, GetFName());
+}
+
+void UItemBase::SetAllowedInteraction(EItemAllowedInteractions Interaction, bool NewState)
+{
+	if (NewState) {
+		AllowedInteractions |= Interaction;
+	} else {
+		AllowedInteractions &= ~Interaction;
+	}
 }
--- Source/AlphaOne/Public/Items/ItemBase.h
+++ Source/AlphaOne/Public/Items/ItemBase.h
@@ -2,14 +2,23 @@
 
 #pragma once
 
-#include "CoreMinimal.h"
+#include "AlphaOne.h"
 #include "Engine/DataAsset.h"
 #include "Styling/SlateBrush.h"
 #include "ItemBase.generated.h"
 
-/**
- * 
- */
+UENUM(BlueprintType, meta = (Bitflags, UseEnumValuesAsMaskValuesInEditor = "true"))
+enum class EItemAllowedInteractions: uint8
+{
+    NONE            = 0          UMETA(Hidden),
+    Stackable       = 1 << 0     UMETA(DisplayName = "Stackable"),
+    Consumable      = 1 << 1     UMETA(DisplayName = "Consumable"),
+	Equipable		= 1 << 2	 UMETA(DisplayName = "Equipable"),
+	Sellable		= 1 << 3	 UMETA(DisplayName = "Sellable"),
+	Droppable		= 1 << 4	 UMETA(DisplayName = "Droppable"),
+};
+ENUM_CLASS_FLAGS(EItemAllowedInteractions);
+
 UCLASS()
 class ALPHAONE_API UItemBase : public UPrimaryDataAsset
 {
@@ -21,55 +30,50 @@ public:
 		: Price(0)
 		, MaxCount(1)
 		, MaxLevel(1)
-		, AbilityLevel(1)
 	{}
 
 	// Type of this item, set in native parent class
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Item)
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Item")
 	FPrimaryAssetType ItemType;
 
 	// User-visible short name
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Item)
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Item")
 	FText ItemName;
 
 	// User-visible long description
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Item)
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Item")
 	FText ItemDescription;
 
 	// Icon to display
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Item)
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Item")
 	FSlateBrush ItemIcon;
 
 	// Price in game
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Item)
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Item")
 	int32 Price;
 
 	// Maximum number of instances that can be in inventory at once, <= 0 means infinite
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Max)
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Max")
 	int32 MaxCount;
 
-	// Returns if the item is consumable (MaxCount <= 0)
-	UFUNCTION(BlueprintCallable, BlueprintPure, Category = Max)
-	bool IsConsumable() const;
-
 	// Maximum level this item can be, <= 0 means infinite
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Max)
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Max")
 	int32 MaxLevel;
 
-	/*
-	// Ability to grant if this item is slotted
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Abilities)
-	TSubclassOf<URPGGameplayAbility> GrantedAbility;
-	*/
-
-	// Ability level this item grants. <= 0 means the character level
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Abilities)
-	int32 AbilityLevel;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Item",  meta = (Bitmask, BitmaskEnum = EUnitActions))
+	EItemAllowedInteractions AllowedInteractions;
 
 	// Returns the logical name, equivalent to the primary asset id
-	UFUNCTION(BlueprintCallable, Category = Item)
+	UFUNCTION(BlueprintCallable, Category = "Item")
 	FString GetIdentifierString() const;
 
 	// Overridden to use saved type
 	virtual FPrimaryAssetId GetPrimaryAssetId() const override;
+
+	UFUNCTION(BlueprintCallable)
+	FORCEINLINE bool CheckAllowedInteraction(EItemAllowedInteractions Interaction) { return static_cast<bool>(AllowedInteractions & Interaction); }
+
+	UFUNCTION(BlueprintCallable)
+	void SetAllowedInteraction(EItemAllowedInteractions Interaction, bool NewState = true);
+
 };
